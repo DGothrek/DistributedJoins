c74aeb26bbbafd9da81a2a6886cf8dcf24bddd41
Pierre <pierre.tholoniat@polytechnique.edu>
2018-05-30 23:28:15 +0200

    test dir

--- testBasicDistributedJoin.cpp
+++ testBasicDistributedJoin.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <mpi.h>
 #include "Relation.h"
+#include "testRelation.cpp"
 
 
 int main (int argc, char *argv[]) {
 
-    /**
-    * Read files
-    **/
+    try{
 
-    Relation rela,Relation relb, vector<int> ensa, vector<int> ensb;//TODO
-    int repartition_variable;
+        /**
+        * Read files
+        **/
 
-
-    /**
-    * Compute a basic distributed join
-    **/
-
-    MPI_Init(&argc, &argv);
-
+        Relation relat_test1 = Relation("data/facebook.dat");
+        Relation relat_test2 = Relation("data/facebook.dat");
+        vector<int> ens1;
+        vector<int> ens2;
+        ens1.push_back(0);
+        ens2.push_back(1);
 
 
-    MPI_Finalize();
+        /**
+        * Compute a basic distributed join
+        **/
+
+        MPI_Init(&argc, &argv);
+
+        Relation relat_test = basicDistributedJoin(relat_test1,relat_test2,ens1,ens2,0);
+
+        MPI_Finalize();
+
+        int size = relat_test.getSize();
+        for(int i=0;i<size;i++){
+            for(int j=0;j<relat_test.getArity();j++){
+                std::cout<<relat_test.getOrderedTuple(i)[j]<<", ";
+            }
+            cout<<endl;
+        }
+    }
+    catch(string const& err){
+        cout<<err<<endl;
+    }
+
+
+
+
+
+
     return 0;
